b6afb472cdd2c44cc5e77f50b8559d500ab77b51
runtyalien <omgadkar@gmail.com>
2024-10-12 15:18:26 +0530

    feat: handle single and two number input

--- calculator.js
+++ calculator.js
@@ -1,7 +1,8 @@
 function add(numbers){
     if(numbers === "") return 0;
 
-    return 0;
+    const numberArray = numbers.split(",");
+    return numberArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
 
 module.exports = add;

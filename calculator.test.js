64ea18a99117dea18898a54fbec9df2ea86e649c
runtyalien <omgadkar@gmail.com>
2024-10-12 15:16:08 +0530

    test: single and two number input

--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,13 @@
-const add = require('./calculator');
+const add = require("./calculator");
 
-test('should return 0 for an empty string', () => {
-  expect(add('')).toBe(0);
+test("should return 0 for an empty string", () => {
+  expect(add("")).toBe(0);
+});
+
+test('should return 1 for the string "1"', () => {
+  expect(add("1")).toBe(1);
+});
+
+test('should return the sum of two numbers', () => {
+    expect(add("1,5")).toBe(6);
 });

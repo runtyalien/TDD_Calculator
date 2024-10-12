65c51cef695a1370dbf75b857fc4e74345a70f25
runtyalien <omgadkar@gmail.com>
2024-10-12 15:27:34 +0530

    feat: handle new line in input

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 function add(numbers){
     if(numbers === "") return 0;
 
-    const numberArray = numbers.split(",");
+    const numberArray = numbers.split(/,|\n/);
     return numberArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
 

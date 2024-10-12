c35d82f24dd5a07549f63db1c3d4102f10084d1d
runtyalien <omgadkar@gmail.com>
2024-10-12 15:50:16 +0530

    test: negative numbers in input

--- calculator.test.js
+++ calculator.test.js
@@ -23,3 +23,11 @@ test('should handle new lines between numbers', () => {
 test('should handle custom delimiters', () => {
     expect(add("//;\n1;2")).toBe(3);
 });
+
+test('should throw an error for negative numbers', () => {
+    expect(() => add("1,-2,3")).toThrow('negative numbers not allowed: -2');
+});
+
+test('should throw an error for multiple negative numbers', () => {
+    expect(() => add("1,-2,-3")).toThrow('negative numbers not allowed: -2,-3');
+});

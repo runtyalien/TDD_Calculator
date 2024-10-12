40d73b87ce6627f3abb9f9a345682e024f1ef17c
runtyalien <omgadkar@gmail.com>
2024-10-12 15:47:54 +0530

    feat: handle custom delimiter

--- calculator.js
+++ calculator.js
@@ -1,7 +1,14 @@
 function add(numbers){
     if(numbers === "") return 0;
 
-    const numberArray = numbers.split(/,|\n/);
+    let delimiter = /,|\n/;
+    if (numbers.startsWith("//")) {
+        const delimiterEnd = numbers.indexOf("\n");
+        delimiter = new RegExp(numbers.substring(2, delimiterEnd));
+        numbers = numbers.substring(delimiterEnd + 1);
+    }
+
+    const numberArray = numbers.split(delimiter);
     return numberArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
 
